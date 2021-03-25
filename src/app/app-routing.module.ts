bcf8833fb736cd3787e010f0af4fa2635b45c41d
ruka.kanno <ruka.kanno@nextbeat.net>
2021-03-25 15:48:47 +0900

    routing: dashboard, root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { HeroesComponent } from "./heroes/heroes.component";
 import { SampleComponent } from './sample/sample.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
-  { path: 'sample', component: SampleComponent }
+  { path: 'sample', component: SampleComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -17,6 +17,6 @@ export class DashboardComponent implements OnInit {
   }
 
   getHeroes() {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1, 5))
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(0, 5))
   }
 }
